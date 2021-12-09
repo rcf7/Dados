cbe5538d4cd8d5622a4af5f8ae870230e4d15a2d
rcf7 <rodrigocampanaferrer@gmail.com>
2021-12-09 08:13:39 -0300

    img

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Dados';
+  dadoIzquierda = "../assets/caras dados/cara1.png"
+  dadoDerecha = "../assets/caras dados/cara4.png"
+  numero1= 1;
+  numero2= 2;
+
+  tirarDados(): void {
+    this.numero1 = Math.round (Math.random() * 5) + 1;
+    this.numero2 = Math.round (Math.random() * 5) + 1;
+    this.dadoIzquierda = '../assets/caras dados/cara' + this.numero1 + '.png';
+    this.dadoDerecha = '../assets/caras dados/cara' + this.numero2 + '.png'
+  }
 }
